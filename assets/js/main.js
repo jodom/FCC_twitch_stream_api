256cedbfad709c70ced5564d94a283cceb814cb9
jodom <jodomouma@gmail.com>
2017-09-09 13:05:03 +0300

    demo stream visualisation using dummy data

---
+++ assets/js/main.js
@@ -0,0 +1,171 @@
+// defaults
+var default_logo = "assets/img/tv.png";
+var offline_status = 'Offline';
+// sample data reply from twicth API
+var data = [
+  {
+    "stream": {
+      "mature": false,
+      "status": "Greg working on Electron-Vue boilerplate w/ Akira #programming #vuejs #electron",
+      "broadcaster_language": "en",
+      "display_name": "FreeCodeCamp",
+      "game": "Creative",
+      "language": "en",
+      "_id": 79776140,
+      "name": "freecodecamp",
+      "created_at": "2015-01-14T03:36:47Z",
+      "updated_at": "2016-09-17T05:00:52Z",
+      "delay": null,
+      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png",
+      "banner": null,
+      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
+      "background": null,
+      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png",
+      "profile_banner_background_color": null,
+      "partner": false,
+      "url": "https://www.twitch.tv/freecodecamp",
+      "views": 161989,
+      "followers": 10048,
+      "_links": {
+        "self": "https://api.twitch.tv/kraken/channels/freecodecamp",
+        "follows": "https://api.twitch.tv/kraken/channels/freecodecamp/follows",
+        "commercial": "https://api.twitch.tv/kraken/channels/freecodecamp/commercial",
+        "stream_key": "https://api.twitch.tv/kraken/channels/freecodecamp/stream_key",
+        "chat": "https://api.twitch.tv/kraken/chat/freecodecamp",
+        "subscriptions": "https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions",
+        "editors": "https://api.twitch.tv/kraken/channels/freecodecamp/editors",
+        "teams": "https://api.twitch.tv/kraken/channels/freecodecamp/teams",
+        "videos": "https://api.twitch.tv/kraken/channels/freecodecamp/videos"
+      }
+    },
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/freecodecamp",
+      "channel": "https://api.twitch.tv/kraken/channels/freecodecamp"
+    }
+  },
+  {
+    "stream": null,
+    "display_name": "OgamingSC2",
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/ogamingsc2",
+      "channel": "https://api.twitch.tv/kraken/channels/ogamingsc2"
+    }
+  },
+  {
+    "stream": {
+      "mature": false,
+      "status": "RERUN: StarCraft 2 - Kane vs. HuK (ZvP) - WCS Season 3 Challenger AM - Match 4",
+      "broadcaster_language": "en",
+      "display_name": "ESL_SC2",
+      "game": "StarCraft II",
+      "language": "en",
+      "_id": 30220059,
+      "name": "esl_sc2",
+      "created_at": "2012-05-02T09:59:20Z",
+      "updated_at": "2016-09-17T06:02:57Z",
+      "delay": null,
+      "logo": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_image-d6db9488cec97125-300x300.jpeg",
+      "banner": null,
+      "video_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-channel_offline_image-5a8657f8393c9d85-1920x1080.jpeg",
+      "background": null,
+      "profile_banner": "https://static-cdn.jtvnw.net/jtv_user_pictures/esl_sc2-profile_banner-f8295b33d1846e75-480.jpeg",
+      "profile_banner_background_color": "#050506",
+      "partner": true,
+      "url": "https://www.twitch.tv/esl_sc2",
+      "views": 60843789,
+      "followers": 135275,
+      "_links": {
+        "self": "https://api.twitch.tv/kraken/channels/esl_sc2",
+        "follows": "https://api.twitch.tv/kraken/channels/esl_sc2/follows",
+        "commercial": "https://api.twitch.tv/kraken/channels/esl_sc2/commercial",
+        "stream_key": "https://api.twitch.tv/kraken/channels/esl_sc2/stream_key",
+        "chat": "https://api.twitch.tv/kraken/chat/esl_sc2",
+        "subscriptions": "https://api.twitch.tv/kraken/channels/esl_sc2/subscriptions",
+        "editors": "https://api.twitch.tv/kraken/channels/esl_sc2/editors",
+        "teams": "https://api.twitch.tv/kraken/channels/esl_sc2/teams",
+        "videos": "https://api.twitch.tv/kraken/channels/esl_sc2/videos"
+      }
+    },
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
+      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
+    }
+  },
+  {
+    "stream": null,
+    "display_name": "noobs2ninjas",
+    "_links": {
+      "self": "https://api.twitch.tv/kraken/streams/esl_sc2",
+      "channel": "https://api.twitch.tv/kraken/channels/esl_sc2"
+    }
+  },
+  {
+    "error": "Not Found",
+    "status": 404,
+    "message": "Channel 'not-a-valid-account' does not exist"
+  }
+];
+
+// real API call goes here
+
+$(document).ready(() => {
+
+	listAllStreamers();
+
+  $("#all").on('click', () => {
+    listAllStreamers();
+  });
+  $("#online").on("click", () => {
+    listOnlineStreamers();
+  });
+  $("#offline").on('click', () => {
+    listOfflineStreamers();
+  });
+
+  var button = $('button');
+
+  button.click( function(){
+    button.removeClass('live');
+    $(this).addClass('live');
+  });
+
+});
+
+function listAllStreamers(){
+	// all accounts
+	var streamers = "";
+  var html = $("#streamer");
+
+	data.forEach(streamer =>{
+		if (streamer.hasOwnProperty("stream")){
+      if (streamer.stream === null){
+        streamers += "<div class='streamer'><div class='image'><img src='"+ default_logo +"' alt='TTV'></div><div class='details'><div class='name'><h3 id='display_name'>"+ streamer.display_name +"</h3></div><div class='playing'><a href='"+ streamer._links.channel +"' alt='channel link' target='_blank'><p id='current'>"+ offline_status +"</p></a></div></div></div>";
+      }else{      
+        streamers += "<div class='streamer'><div class='image'><img src='"+ streamer.stream.logo +"' alt='TTV'></div><div class='details'><div class='name'><h3 id='display_name'>"+ streamer.stream.display_name +"</h3></div><div class='playing'><a href='"+ streamer._links.channel +"' alt='channel link' target='_blank'><p id='current'>"+ streamer.stream.status +"</p></a></div></div></div>"; 
+      }
+    }
+  });
+  $("#content").html(streamers);
+}
+
+function listOnlineStreamers(){
+  // filter for online accounts
+  var online = data.filter( streamer => {return streamer.stream });
+
+  var streamers = "";
+  online.forEach(streamer =>{
+    streamers += "<div class='streamer'><div class='image'><img src='"+ streamer.stream.logo +"' alt='TTV'></div><div class='details'><div class='name'><h3 id='display_name'>"+ streamer.stream.display_name +"</h3></div><div class='playing'><a href='"+ streamer._links.channel +"' alt='channel link' target='_blank'><p id='current'>"+ streamer.stream.status +"</p></a></div></div></div>"; 
+  });
+  $("#content").html(streamers);
+}
+
+function listOfflineStreamers(){
+  // filter for offline accounts
+  var offline = data.filter( streamer => {return streamer.stream === null });
+
+  var streamers = "";
+  offline.forEach(streamer =>{
+    streamers += "<div class='streamer'><div class='image'><img src='"+ default_logo +"' alt='TTV'></div><div class='details'><div class='name'><h3 id='display_name'>"+ streamer.display_name +"</h3></div><div class='playing'><a href='"+ streamer._links.channel +"' alt='channel link' target='_blank'><p id='current'>"+ offline_status +"</p></a></div></div></div>";
+  });
+  $("#content").html(streamers);
+}
